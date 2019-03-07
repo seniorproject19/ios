7266ea75ef6d5836fcf516b390ef5a680f0e8734
Jiaqing Mo <jimo@calpoly.edu>
2019-03-07 15:52:55 -0800

    postTime

--- SeniorProject/Controllers/NewPost/PostDetail/AddTimeSlotViewController.swift
+++ SeniorProject/Controllers/NewPost/PostDetail/AddTimeSlotViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class AddTimeSlotViewController: UIViewController , UIPickerViewDataSource, UIPickerViewDelegate{
+class AddTimeSlotViewController: UIViewController , UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate{
    
+
     
 
     @IBOutlet weak var timePicker: UIDatePicker!
@@ -22,14 +23,23 @@ class AddTimeSlotViewController: UIViewController , UIPickerViewDataSource, UIPi
     @IBOutlet weak var endTimeButton: UIButton!
     @IBOutlet weak var startTimeButton: UIButton!
     @IBOutlet weak var selectDayButton: UIButton!
+
+    // Now we specify the display format, e.g. "27-08-2015
     
     let weekDayPickerOptions = ["Monday","Tuesday","Wednesday","Thursday","Friday", "Saturday", "Sunday"]
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
+        timePicker.tag = 0
+        weekdayPicker.tag = 1
+        selectDayTextField.tag = 2
+        startTimeTextField.tag = 3
+        endTimeTextField.tag = 4
+        timePicker.isHidden = true
+        weekdayPicker.isHidden = true
         // Do any additional setup after loading the view.
     }
-    
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -41,6 +51,47 @@ class AddTimeSlotViewController: UIViewController , UIPickerViewDataSource, UIPi
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return weekDayPickerOptions[row]
     }
+    
+
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if (textField.tag == 2){
+            timePicker.isHidden = true
+            weekdayPicker.isHidden = false
+        }
+        else {
+            timePicker.isHidden = false
+            weekdayPicker.isHidden = true
+        }
+        return true
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectDayTextField.text = weekDayPickerOptions[row]
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if (textField.tag == 3){
+            
+            let dateFormatr = DateFormatter()
+            dateFormatr.dateFormat = "h:mm a"
+            let strDate = dateFormatr.string(from: (timePicker?.date)!)
+            textField.text = strDate
+            print("3 ended")
+            
+        }
+        else if (textField.tag == 4){
+            let dateFormatr = DateFormatter()
+            dateFormatr.dateFormat = "h:mm a"
+            let strDate = dateFormatr.string(from: (timePicker?.date)!)
+            textField.text = strDate
+            print("4 ended")
+        }
+        return true
+    }
+
+        
+    
+    
     /*
     // MARK: - Navigation
 
